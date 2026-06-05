b2bf9cf9a5af49661737d44b34e3b6e66f689f20
Mintu Kumar <mintuchamp@MINTUs-MacBook-Air.local>
2026-06-05 15:03:13 +0530

    fix: resolve server

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ad-clip-ai.vercel.app"],
     credentials: true,
   }),
 );
